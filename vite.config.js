669b4ab0c07a12b68c02c5fb208e6ed35a38ea43
Osland07 <111648250+Osland07@users.noreply.github.com>
2025-06-29 21:52:26 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',        // agar bisa diakses dari perangkat lain
+        port: 5173,             // port default Vite
+        hmr: {
+            host: '192.168.18.40', // ganti pake IP lokal laptop
+        },
+    },
     plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
+    ],
 });
