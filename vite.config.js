c2172f4e5d403283c2810b57ec0aa491414109f4
Osland07 <111648250+Osland07@users.noreply.github.com>
2025-06-30 22:17:26 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,33 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import os from 'os';
+
+function getLocalIP() {
+    const interfaces = os.networkInterfaces();
+    for (const name of Object.keys(interfaces)) {
+        for (const iface of interfaces[name]) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return '127.0.0.1';
+}
+
+const localIp = getLocalIP();
 
 export default defineConfig({
     server: {
-        host: '0.0.0.0',        // agar bisa diakses dari perangkat lain
-        port: 5173,             // port default Vite
+        host: '0.0.0.0',
+        port: 5173,
         hmr: {
-            host: '10.11.180.15', // ganti pake IP lokal laptop
+            host: localIp,
         },
     },
     plugins: [
         laravel([
             'resources/css/app.css',
             'resources/js/app.js',
-        ]),
-    ],
+        ]),
+    ],
 });
