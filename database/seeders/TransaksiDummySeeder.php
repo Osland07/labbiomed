d5e4961b07ccc748a37813cfce1e75ac66deb131
Osland07 <111648250+Osland07@users.noreply.github.com>
2025-07-01 21:15:53 +0700

    Update TransaksiDummySeeder.php

--- database/seeders/TransaksiDummySeeder.php
+++ database/seeders/TransaksiDummySeeder.php
@@ -16,8 +16,8 @@ class TransaksiDummySeeder extends Seeder
 {
     public function run(): void
     {
-        $start = Carbon::now()->subDays(180);
-        $end = Carbon::now()->addDays(185);
+        $start = Carbon::now()->subDays(30);
+        $end = Carbon::now()->addDays(30);
         $dates = [];
         for ($date = $start->copy(); $date->lte($end); $date->addDay()) {
             if ($date->isWeekday()) {
@@ -32,7 +32,7 @@ class TransaksiDummySeeder extends Seeder
         $ruangans = Ruangan::all();
 
         // Hitung jumlah data per hari per fitur
-        $dataPerHari = 100; // jumlah data per hari per fitur
+        $dataPerHari = 5; // jumlah data per hari per fitur
 
         foreach ($dates as $date) {
             // --- Peminjaman ---
