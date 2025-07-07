b0b96b891fd6de560f9eb662e04748a9379ed909
Osland07 <111648250+Osland07@users.noreply.github.com>
2025-07-07 23:04:03 +0700

    Update index.blade.php

--- resources/views/client/penggunaan-alat/index.blade.php
+++ resources/views/client/penggunaan-alat/index.blade.php
@@ -82,7 +82,7 @@
                 <form id="formPenggunaanAlat" method="POST" action="{{ route('client.penggunaan-alat.store') }}">
                     @csrf
                     <div class="modal-header">
-                        <h5 class="modal-title" id="modalPenggunaanLabel">Ajukan Penggunaan Alat</h5>
+                        <h5 class="modal-title" id="modalPenggunaanLabel">Gunakan Alat</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
